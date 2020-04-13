86d17b217e5339b4c69f2045c31847ada98bb52c
Max Sautia <saula@niutechtuvalu.com>
2020-04-14 02:52:14 +1200

    Updates

---
+++ precache-manifest.fba6c5b779c30c0a12d0dff25a806fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920588574bd4084e0b2df31af98f3952",
+    "url": "/tapapa-app/index.html"
+  },
+  {
+    "revision": "060c41afd5b3c29d6423",
+    "url": "/tapapa-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91f2ae2a366ddbbd6876",
+    "url": "/tapapa-app/static/js/2.39b38d1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tapapa-app/static/js/2.39b38d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060c41afd5b3c29d6423",
+    "url": "/tapapa-app/static/js/main.4d031892.chunk.js"
+  },
+  {
+    "revision": "330f89218677460e5f6b",
+    "url": "/tapapa-app/static/js/runtime-main.04c56621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tapapa-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
